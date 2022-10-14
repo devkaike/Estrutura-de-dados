b7fdfd4f5ebd08061d989ad2bf3fadfa1dfd7e06
Kaike silva <107559196+devkaike@users.noreply.github.com>
2022-10-14 20:12:57 -0300

    Add files via upload

---
+++ Livro.java
@@ -0,0 +1,12 @@
+
+public class Livro {
+    private String titulo, autor, editora;
+    public Livro(String t, String a, String e){
+        titulo = t;
+        autor = a;
+        editora = e;
+    }
+    public String getLivro(){
+        return (titulo + "\n" + autor + "\n" + editora + "\n");
+    }
+}
